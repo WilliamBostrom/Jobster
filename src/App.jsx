96ab3104cb5e28cd039e18889c314147feebec5c
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-05-01 22:40:06 +0200

    setup register redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Dashboard, Error, Landing, Register } from "./pages";
+import { Dashboard, Error, Landing, Register } from "./pages/index";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
@@ -7,10 +9,13 @@ const App = () => {
       <Routes>
         <Route path="/landing" element={<Landing />} />
         <Route path="/" element={<Dashboard />} />
-        <Route path="/Register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <ToastContainer position="top-center text-center" />
     </BrowserRouter>
   );
 };
+
+//Toastify__toast för css
 export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,82 @@
+import { useEffect, useState } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
+
+const initalState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initalState);
+  const dispatch = useDispatch();
+  const { user, isLoading } = useSelector((store) => store.user);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}:${value}`);
+    setValues({ ...values, [name]: value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      console.log("fyll i alla fält");
+      toast.error("Skriv i alla fälten");
+      return;
+    }
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+      <p>{values.isMember ? "Not a member yet?" : "Already a member?"}</p>
+      <button className="member-btn" onClick={toggleMember}>
+        {values.isMember ? "Register" : "Login"}
+      </button>
+    </Wrapper>
+  );
 }
 
 export default Register;
